7f99d2c350a61aeafded0cce1e2c2fae77d1d591
Matheus Meireles <matheus.meireles910@gmail.com>
2024-01-10 22:00:09 -0300

    função média e situação

--- SistemaAluno/src/cursojava/executavel/PrimeiraClasseJava.java
+++ SistemaAluno/src/cursojava/executavel/PrimeiraClasseJava.java
@@ -7,7 +7,8 @@ public class PrimeiraClasseJava {
 	    /*Main é um método auto executavel no JAVA*/
 	    public static void main(String[] args) {
 	        // TODO code application logic here
-	    		        
+	    	
+	    	/*Processamento de dados e atribuição ao objeto*/
 	        String nome = JOptionPane.showInputDialog("Qual o nome do aluno?");
 	        int idade = JOptionPane.showInputDialog("Informe a idade");
 		    String numeroCpf = JOptionPane.showInputDialog("Informe o CPF");
@@ -24,7 +25,24 @@ public class PrimeiraClasseJava {
 	    	cursojava.classes.Aluno aluno1 = new cursojava.classes.Aluno();
 	        		
     		
-	        aluno1.nome = "Matheus";	        
-	        System.out.println("O aluno está: "+aluno1.getSituacao());
+	        aluno1.setNome (nome);
+	        aluno1.setIdade(Integer.valueOf(idade));
+	        aluno1.setNumeroCpf (numeroCpf);
+	        aluno1.setNomePai (nomePai);
+	        aluno1.setNota1 (Double.parseDouble(nota1));
+	        aluno1.setNota2 (Double.parseDouble(nota2));
+	        aluno1.setNota3 (Double.parseDouble(nota3));
+	        aluno1.setNota4 (Double.parseDouble(nota4));
+	        
+	        //
+	        System.out.println("O aluno: "+aluno1.getNome());
+	        System.out.println("Idade: "+aluno1.getIdade());
+	        System.out.println("Cpf:"+aluno1.getNumeroCpf());
+	        System.out.println("Nome pai: "+aluno1.getNomePai());
+	        System.out.println("");
+	        System.out.println("Média final: "+aluno1.getMediaNota());
+	        System.out.println("Está com situação final: "+aluno1.getSituacao());
+	        System.out.println("toString: " + aluno1);
+	        break;
 	    }
 }
